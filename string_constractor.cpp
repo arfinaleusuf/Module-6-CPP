e0347bfe899e9deb79633ed2d681131003fbae08
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-27 22:38:46 +0600

    string constractor

---
+++ string_constractor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s = "hello";
+    // 1
+    // string s("Hello");
+    // 2
+    // string s("Hello World",4);
+    // 3
+    // string s("Hello World");
+    // string t(s,4);
+    // 4 
+    string s(5,'a');
+    cout<< s << endl;
+    return 0;
+}
